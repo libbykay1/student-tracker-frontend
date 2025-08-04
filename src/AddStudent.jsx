d00c700dec091f66954b56bfa25af480f83039a6
Libby Eilbert <elizabeth.eilbert@gmail.com>
2025-08-04 16:31:11 -0700

    fixed useEffect on addStudent

--- src/AddStudent.jsx
+++ src/AddStudent.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const API_BASE = import.meta.env.VITE_BACKEND_URL;
